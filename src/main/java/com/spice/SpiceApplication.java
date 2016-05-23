3755cbcd6bf16d5f84a74e4035eee7ad202260fe
U33843 <U33843@F703DTRV.ustr.com>
2016-05-23 14:44:36 +0530

    allow origin  *

--- src/main/java/com/spice/SpiceApplication.java
+++ src/main/java/com/spice/SpiceApplication.java
@@ -161,8 +161,7 @@ class CsrfHeaderFilter extends OncePerRequestFilter {
 	    		response.addCookie(cookie);
 	    	}
 	    }
-	    
-	    response.setHeader("Access-Control-Allow-Origin", "http://localhost:8084");
+	    response.setHeader("Access-Control-Allow-Origin", "*");
 	    response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
 	    response.setHeader("Access-Control-Max-Age", "3600");
 	    response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
